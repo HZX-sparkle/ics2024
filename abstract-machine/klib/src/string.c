e8e34aa9647bde3d7b213b155b41203557ecd608
tracer-ics2024 <tracer@njuics.org>
2024-10-23 23:17:12 +0800

    >  compile NEMU 231220000 张三 Linux kali-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:17:12 up 39 min,  1 user,  load average: 0.66, 0.61, 0.47

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -9,7 +9,6 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-  printf("strcpy\n");
   char *p = dst;
   while(!*src) *p++ = *src++;
   *p = 0;
@@ -22,7 +21,6 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  printf("strcat\n");
   char *p = dst;
   while(!*p) p++;
   while(!*src) *p++ = *src++;
@@ -32,7 +30,6 @@ char *strcat(char *dst, const char *src) {
 }
 
 int strcmp(const char *s1, const char *s2) {
-  printf("strcmp\n");
   while (1)
   {
     char c1 = *s1++;
@@ -48,7 +45,6 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  printf("memset\n");
   unsigned char* p = s;
   unsigned char cc = (unsigned char)c;
   for (size_t i = 0; i < n; i++, p++)
@@ -67,7 +63,6 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  printf("memcmp\n");
   while (n--)
   {
     unsigned char c1 = *(unsigned char*)s1;
