0ea8d12cc0d0ec68d95410742bcd24e5eb9b15f5
tracer-ics2024 <tracer@njuics.org>
2024-10-24 00:55:41 +0800

    >  compile NEMU 231220000 张三 Linux kali-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:55:41 up  2:17,  1 user,  load average: 0.37, 0.49, 0.54

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,48 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list args;
+  va_start(args, fmt);
+  const char *p = fmt;
+  char *dst = out;
+  while (*p)
+  {
+    if(*p == '%')
+    {
+      switch (*++p)
+      {
+      case 'd':
+        int val = va_arg(args, int);
+        char buf[20];
+        int i = 0;
+        if (val < 0)
+        {
+          *dst++ = '-';
+          val = -val;
+        }
+        do
+        {
+          buf[i++] = val%10 + '0';
+          val /= 10;
+        } while (val != 0);
+        for (i = i - 1; i >=0 ; i--) *dst++ = buf[i];
+        break;
+      
+      case 's':
+        char *s = va_arg(args, char*);
+        while((*dst++ = *s++));
+        break;
+
+      default:
+        *dst++ = '%';
+        *dst++ = *p;
+        break;
+      }
+    } else *dst++ = *p;
+    p++;
+  }
+  return dst-out;
+  // panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
