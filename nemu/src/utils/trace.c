7ef332495d2c05cdaec555d4a29df6bec345dc7c
tracer-ics2024 <tracer@njuics.org>
2024-11-03 12:33:54 +0800

    >  compile NEMU 231220000 张三 Linux kali-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:33:54 up 27 min,  1 user,  load average: 0.10, 0.09, 0.18

---
+++ nemu/src/utils/trace.c
@@ -0,0 +1,22 @@
+#include <common.h>
+
+#define MAX_INST 15
+
+char *iringbuf[MAX_INST];
+
+int begin, cur = 0;
+
+void store_inst(char *buf)
+{
+    strcpy(iringbuf[cur], buf);
+    cur = (cur + 1) % MAX_INST;
+    if (cur == begin)
+        begin = (begin + 1) % MAX_INST;
+}
+
+void display_inst()
+{
+    for (int i = 0; i != cur; i = (i + 1) % MAX_INST)
+        printf("    %s\n", iringbuf[i]);
+    printf(" -->%s\n", iringbuf[cur]);
+}
