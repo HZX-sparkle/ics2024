4706dd8b7a9b0b0f70cb9d4850f22c3b6399271b
tracer-ics2024 <tracer@njuics.org>
2024-11-03 12:37:56 +0800

    >  compile NEMU 231220000 张三 Linux kali-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:37:56 up 31 min,  1 user,  load average: 0.00, 0.03, 0.13

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -50,6 +50,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc)
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   IFDEF(CONFIG_WATCHPOINT, check_wp());
   // iringbuf: store instructions.
+  assert(0);
   IFDEF(CONFIG_ITRACE, store_inst(_this->logbuf));
 }
 
