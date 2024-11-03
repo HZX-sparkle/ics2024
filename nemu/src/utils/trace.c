ccf95a2fdcecdc4243382f7190933daf31cf5860
tracer-ics2024 <tracer@njuics.org>
2024-11-03 12:57:28 +0800

    >  compile NEMU 231220000 张三 Linux kali-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:57:28 up 51 min,  1 user,  load average: 0.06, 0.28, 0.31

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -1,15 +1,19 @@
 #include <common.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define MAX_INST 15
+#define MAX_INST_SIZE 100
 
-char *iringbuf[MAX_INST] = {0};
+char iringbuf[MAX_INST][MAX_INST_SIZE];
 
 int begin, cur = 0;
 
 void store_inst(char *buf)
 {
-    strcpy(iringbuf[cur], buf);
+    strncpy(iringbuf[cur], buf, MAX_INST_SIZE - 1);
     assert(0);
+    iringbuf[cur][MAX_INST_SIZE - 1] = 0;
     cur = (cur + 1) % MAX_INST;
     if (cur == begin)
         begin = (begin + 1) % MAX_INST;
