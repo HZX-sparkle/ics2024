c7ca6eed4c93f40164477aa8aa6c165e95363e9b
tracer-ics2024 <tracer@njuics.org>
2024-11-11 15:28:24 +0800

    >  compile NEMU 231220000 张三 Linux kali-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:28:24 up  3:30,  1 user,  load average: 0.31, 0.10, 0.10

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -69,10 +69,10 @@ void init_elf(const char *elf_file)
     } else return;
     Log("Elf file at %s", elf_file);
     // read elf header
-    assert(0);
     Assert(fread(&ehdr, sizeof(Elf32_Ehdr), 1, elf_fp) == 1, "Cannot read elf header");
 
     // read all section headers to shdr
+    assert(0);
     shdr = malloc(ehdr.e_shnum * sizeof(Elf32_Shdr));
     // set fp to section headers
     fseek(elf_fp, ehdr.e_shoff, SEEK_SET);
