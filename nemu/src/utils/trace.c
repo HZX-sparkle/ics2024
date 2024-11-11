b34fa6863e7c39bcc543bfcd8da560b3ee204538
tracer-ics2024 <tracer@njuics.org>
2024-11-11 12:01:26 +0800

    >  compile NEMU 231220000 张三 Linux kali-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:01:26 up 32 min,  1 user,  load average: 0.07, 0.12, 0.32

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -1,14 +1,12 @@
 #include <common.h>
-#include <string.h>
-#include <stdlib.h>
+#include <elf.h>
 
 #define MAX_INST 15
 #define MAX_INST_SIZE 100
 
+// iringbuf
 char iringbuf[MAX_INST][MAX_INST_SIZE];
-
 int begin, cur = 0;
-
 void store_inst(char *buf)
 {
     strncpy(iringbuf[cur], buf, MAX_INST_SIZE - 1);
@@ -29,6 +27,7 @@ void display_inst()
     }
 }
 
+// mtrace
 void mtrace_read(paddr_t addr, int len)
 {
     printf("r: addr = " FMT_PADDR ","
@@ -41,4 +40,100 @@ void mtrace_write(paddr_t addr, int len, word_t data)
     printf("w: addr = " FMT_PADDR ","
            " len = %d, data = " FMT_WORD "\n",
            addr, len, data);
+}
+
+// ftrace
+static FILE* elf_fp = NULL;
+static Elf32_Ehdr ehdr;
+static Elf32_Shdr *shdr;
+static Elf32_Sym *symtab;
+static char *strtab;
+typedef struct func_map
+{
+    char name[256];
+    paddr_t start;
+    word_t size;
+    struct func_map* next; 
+} FM;
+static FM* head = NULL;
+
+void gen_fm(word_t count);
+void init_elf(const char *elf_file)
+{
+    if (elf_file != NULL)
+    {
+        FILE *fp = fopen(elf_file, "rb");
+        Assert(fp, "Can not open '%s'", elf_file);
+        elf_fp = fp;
+    }
+    // read elf header
+    Assert(fread(&ehdr, sizeof(Elf32_Ehdr), 1, elf_fp) == 1, "Cannot read elf header");
+
+    // read all section headers to shdr
+    shdr = malloc(ehdr.e_shnum * sizeof(Elf32_Shdr));
+    // set fp to section headers
+    fseek(elf_fp, ehdr.e_shoff, SEEK_SET);
+    Assert(fread(shdr, sizeof(Elf32_Shdr), ehdr.e_shnum, elf_fp) == ehdr.e_shnum, "Cannot read seciton header");
+
+    // get shstrtab_hdr whose section(section header string table) stores the name of other sections
+    Elf32_Shdr shstrtab_hdr = shdr[ehdr.e_shstrndx];
+    char *shstrtab = malloc(shstrtab_hdr.sh_size);
+    // set fp to section header string table
+    fseek(elf_fp, shstrtab_hdr.sh_offset, SEEK_SET);
+    Assert(fread(shstrtab, shstrtab_hdr.sh_size, 1, elf_fp) == 1, "Cannot read seciton header string table");
+
+    // find .strtab and .symtab
+    for (size_t i = 0; i < ehdr.e_shnum; i++)
+    {
+        char *name = &shstrtab[shdr[i].sh_name];
+        if (strcmp(name, ".strtab") == 0)
+        {
+            Elf32_Shdr strtab_hdr = shdr[i];
+            strtab = malloc(strtab_hdr.sh_size);
+            fseek(elf_fp, strtab_hdr.sh_offset, SEEK_SET);
+            Assert(fread(strtab, strtab_hdr.sh_size, 1, elf_fp) == 1, "Cannot read string table");
+        }
+        if (strcmp(name, ".symtab") == 0)
+        {
+            Elf32_Shdr symtab_hdr = shdr[i];
+            symtab = malloc(symtab_hdr.sh_size);
+            fseek(elf_fp, symtab_hdr.sh_offset, SEEK_SET);
+            Assert(fread(symtab, symtab_hdr.sh_size, 1, elf_fp) == 1, "Cannot read symbol table");
+            gen_fm(symtab_hdr.sh_size / sizeof(Elf32_Sym));
+        }
+    }
+    Assert(symtab, "Failed to find symbol table");
+    Assert(strtab, "Failed to find string table");
+    free(shdr);
+    free(shstrtab);
+    fclose(elf_fp);
+}
+
+void gen_fm(word_t count)
+{
+    for (size_t i = 0; i < count; i++)
+    {
+        Elf32_Sym *sym = &symtab[i];
+        if (ELF32_ST_TYPE(sym->st_info) == STT_FUNC)
+        {
+            FM* fm = malloc(sizeof(FM));
+            char *name = &strtab[sym->st_name];
+            strcpy(fm->name, name);
+            fm->start = sym->st_value;
+            fm->size = sym->st_size;
+            fm->next = head;
+            head = fm;
+        }
+    }
+}
+
+char *match_func(paddr_t addr)
+{
+    FM* fm = head;
+    while (!fm)
+    {
+        if(addr >= fm->start && addr < fm->start + fm->size)  return fm->name;
+        fm = fm->next;
+    }
+    return 0;
 }
