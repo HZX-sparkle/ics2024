062a30fcb0ee353699272e8d80703b097ece10fe
tracer-ics2024 <tracer@njuics.org>
2024-11-11 13:01:18 +0800

    >  compile NEMU 231220000 张三 Linux kali-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:01:18 up  1:03,  1 user,  load average: 1.09, 0.66, 0.34

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -63,6 +63,7 @@ void init_elf(const char *elf_file)
     assert(0);
     if (elf_file != NULL)
     {
+        assert(0);
         FILE *fp = fopen(elf_file, "rb");
         Assert(fp, "Can not open '%s'", elf_file);
         elf_fp = fp;
