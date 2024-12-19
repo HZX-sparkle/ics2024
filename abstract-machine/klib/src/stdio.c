35342cb3cc679535052c7e25b778e13b323ce696
tracer-ics2024 <tracer@njuics.org>
2024-12-19 20:06:37 +0800

    >  compile NEMU 231220000 张三 Linux kali-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:06:37 up 20 min,  1 user,  load average: 0.24, 0.24, 0.24

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,47 +5,99 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+#define MAX_LEN 100
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  char buf[100];
+  int len = vsprintf(buf, fmt, ap);
+  for (const char *p = buf; *p; p++) {
+    putch(*p);
+  }
+  return len;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
-int sprintf(char *out, const char *fmt, ...) {
-  va_list args;
-  va_start(args, fmt);
   const char *p = fmt;
   int len = 0;
   while (*p)
   {
     if (*p == '%')
     {
-      switch (*++p)
+      p++;
+      // 位宽实现
+      int width = atoi(p);
+      char padding = ' ';
+      int n = 0; // 填充位数
+      if (width != 0)
+      {
+        // 0填充和空格填充
+        if(*++p == '0') padding = '0';
+        while ( *p >= '0' && *p <= '9') p++;
+      }
+      
+      switch (*p)
       {
       case 'd':
-        int val = va_arg(args, int);
-        char buf[20] = {0};
-        int i = 0;
-        if (val<0)
+        int dval = va_arg(ap, int);
+        char dbuf[20] = {0};
+        int di = 0;
+        bool isNeg = 0;
+        if (dval < 0)
         {
           out[len++] = '-';
-          val = -val;
+          isNeg = 1;
+          dval = -dval;
         }
         
         do
         {
-          buf[i++] = val%10 + '0';
-          val /= 10;
-        } while (val);
-
-        while (i) out[len++] = buf[--i];
-
+          dbuf[di++] = (dval%10) + '0';
+          dval /= 10;
+        } while (dval);
+        
+        if (isNeg)
+        {
+          n = width-di-1;
+        } else n = width-di;
+        
+        if (n > 0)
+        {
+          while(n--) out[len++] = padding;
+        }
+        while(di) out[len++] = dbuf[--di];
+        
         break;
       
+      case 'x':
+        const char hex[] = "0123456789abcdef";
+        unsigned int xval = va_arg(ap, unsigned int);
+        char xbuf[20];
+        int xi = 0;
+        if(xval == 0) xbuf[xi++] = '0';
+        else {
+          while (xval)
+          {
+            xbuf[xi++] = hex[xval & 0xf];
+            xval >>= 4;
+          }
+        }
+        n = width - xi;
+        if (n > 0)
+        {
+          while(n--) out[len++] = padding;
+        }
+        while (xi) out[len++] = xbuf[--xi];
+        break;
+
       case 's':
-        const char* s = va_arg(args, const char*);
+        const char* s = va_arg(ap, const char*);
+        n = width - strlen(s);
+        if (n > 0)
+        {
+          while(n--) out[len++] = padding;
+        }
         while (*s) out[len++] = *s++;
         break;
 
@@ -58,10 +110,21 @@ int sprintf(char *out, const char *fmt, ...) {
     p++;
   }
   out[len] = 0;
-  va_end(args);
+  while (*out)
+  {
+    putch(*out);
+    out++;
+  }
+  putch('\n');
+  va_end(ap);
   return len;
+}
 
-  // panic("Not implemented");
+int sprintf(char *out, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  int len = vsprintf(out, fmt, ap);
+  return len;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
