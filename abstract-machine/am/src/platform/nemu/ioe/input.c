a82d645dde69e20eb24e7956a43def13ffbe92f8
tracer-ics2024 <tracer@njuics.org>
2024-12-20 21:32:34 +0800

    >  compile NEMU 231220000 张三 Linux kali-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:32:34 up  4:24,  1 user,  load average: 0.15, 0.19, 0.17

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -7,7 +7,6 @@
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t k = AM_KEY_NONE;
   k = inl(KBD_ADDR);
-  if((KEYDOWN_MASK & k) != 0) printf("k = 0x%04x\n", k); 
   kbd->keydown = (KEYDOWN_MASK & k) ? true : false;
   kbd->keycode = k & ~KEYDOWN_MASK;
 }
