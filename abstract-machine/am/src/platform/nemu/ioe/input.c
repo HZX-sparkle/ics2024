df6f0fce6dbdd0c91be64e8c0022c337a6a6edb2
tracer-ics2024 <tracer@njuics.org>
2024-12-20 20:54:20 +0800

    >  compile NEMU 231220000 张三 Linux kali-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:54:20 up  3:46,  1 user,  load average: 0.09, 0.06, 0.14

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,11 +1,13 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdio.h>
 
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t k = AM_KEY_NONE;
   k = inl(KBD_ADDR);
+  printf("0x%04x\n", k);
   kbd->keydown = (KEYDOWN_MASK & k) ? true : false;
   kbd->keycode = k & ~KEYDOWN_MASK;
 }
