f6309431a463b83167952665e9c69dcaf89448eb
tracer-ics2024 <tracer@njuics.org>
2024-12-20 18:26:48 +0800

    >  compile NEMU 231220000 张三 Linux kali-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:26:48 up  1:18,  1 user,  load average: 0.10, 0.10, 0.24

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -52,7 +52,16 @@ void *memset(void *s, int c, size_t n) {
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  unsigned char *p = dst;
+  unsigned char tmp[n];
+  int i = 0;
+  while (n--)
+  {
+    unsigned char c = *(unsigned char*)src;
+    tmp[i++] = c;
+  }
+  while (i--) *p++ = tmp[n++];
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
